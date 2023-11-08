a9c2e34f59c93c6ee60fb3acb3b46f866a714b50
lgswin <lgswin@gmail.com>
2023-11-08 17:40:13 -0500

    fileOps with async

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const fs = require("fs");
+const fsPromises = require("fs").promises;
 
 // fs.readFile("./files/starter.txt", (err, data) => {
 //   if (err) throw err;
@@ -11,43 +11,75 @@ const fs = require("fs");
 // });
 
 const path = require("path");
-fs.readFile(
-  path.join(__dirname, "files", "starter.txt"),
-  "utf8",
-  (err, data) => {
-    if (err) throw err;
-    console.log(data);
-  }
-);
-// async task라는 file읽는동안 hello..를 먼저 출력해버림
-console.log("Hello...");
-
-fs.writeFile(
-  path.join(__dirname, "files", "reply.txt"),
-  "Nice to meet you.",
-  (err) => {
-    if (err) throw err;
-    console.log("write complete");
-
-    fs.appendFile(
-      path.join(__dirname, "files", "reply.txt"),
-      "\n\nYes it is.",
-      (err) => {
-        if (err) throw err;
-        console.log("Append complete");
-
-        fs.rename(
-          path.join(__dirname, "files", "reply.txt"),
-          path.join(__dirname, "files", "newReply.txt"),
-          (err) => {
-            if (err) throw err;
-            console.log("Rename complete");
-          }
-        );
-      }
+const fileOps = async () => {
+  try {
+    const data = await fsPromises.readFile(
+      path.join(__dirname, "files", "starter.txt"),
+      "utf8"
     );
+    console.log(data);
+    await fsPromises.unlink(path.join(__dirname, "files", "starter.txt"));
+    await fsPromises.writeFile(
+      path.join(__dirname, "files", "promiseWrite.txt"),
+      data
+    );
+    await fsPromises.appendFile(
+      path.join(__dirname, "files", "promiseWrite.txt"),
+      "\n\nNice to meet you."
+    );
+    await fsPromises.rename(
+      path.join(__dirname, "files", "promiseWrite.txt"),
+      path.join(__dirname, "files", "promiseComplete.txt")
+    );
+    const newData = await fsPromises.readFile(
+      path.join(__dirname, "files", "promiseComplete.txt"),
+      "utf8"
+    );
+    console.log(newData);
+  } catch (err) {
+    console.error(err);
   }
-);
+};
+
+fileOps();
+
+// fs.readFile(
+//   path.join(__dirname, "files", "starter.txt"),
+//   "utf8",
+//   (err, data) => {
+//     if (err) throw err;
+//     console.log(data);
+//   }
+// );
+// async task라는 file읽는동안 hello..를 먼저 출력해버림
+// console.log("Hello...");
+
+// fs.writeFile(
+//   path.join(__dirname, "files", "reply.txt"),
+//   "Nice to meet you.",
+//   (err) => {
+//     if (err) throw err;
+//     console.log("write complete");
+
+//     fs.appendFile(
+//       path.join(__dirname, "files", "reply.txt"),
+//       "\n\nYes it is.",
+//       (err) => {
+//         if (err) throw err;
+//         console.log("Append complete");
+
+//         fs.rename(
+//           path.join(__dirname, "files", "reply.txt"),
+//           path.join(__dirname, "files", "newReply.txt"),
+//           (err) => {
+//             if (err) throw err;
+//             console.log("Rename complete");
+//           }
+//         );
+//       }
+//     );
+//   }
+// );
 
 // exit on uncaught errors
 process.on("uncaughtException", (err) => {
