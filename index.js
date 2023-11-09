c4a9369f01f50086ef61292618cb06d42f53f3bc
lgswin <lgswin@gmail.com>
2023-11-09 10:17:19 -0500

    gathering log event

--- index.js
+++ index.js
@@ -1,93 +1,16 @@
-const fsPromises = require("fs").promises;
+const logEvents = require("./logEvents");
 
-// fs.readFile("./files/starter.txt", (err, data) => {
-//   if (err) throw err;
-//   console.log(data.toString());
-// });
+const EventEmitter = require("events");
 
-// fs.readFile("./files/starter.txt", "utf8", (err, data) => {
-//   if (err) throw err;
-//   console.log(data);
-// });
+class MyEmitter extends EventEmitter {}
 
-const path = require("path");
-const fileOps = async () => {
-  try {
-    const data = await fsPromises.readFile(
-      path.join(__dirname, "files", "starter.txt"),
-      "utf8"
-    );
-    console.log(data);
-    await fsPromises.unlink(path.join(__dirname, "files", "starter.txt"));
-    await fsPromises.writeFile(
-      path.join(__dirname, "files", "promiseWrite.txt"),
-      data
-    );
-    await fsPromises.appendFile(
-      path.join(__dirname, "files", "promiseWrite.txt"),
-      "\n\nNice to meet you."
-    );
-    await fsPromises.rename(
-      path.join(__dirname, "files", "promiseWrite.txt"),
-      path.join(__dirname, "files", "promiseComplete.txt")
-    );
-    const newData = await fsPromises.readFile(
-      path.join(__dirname, "files", "promiseComplete.txt"),
-      "utf8"
-    );
-    console.log(newData);
-  } catch (err) {
-    console.error(err);
-  }
-};
+// initialize object
+const myEmitter = new MyEmitter();
 
-fileOps();
+// add listeners for the log event
+myEmitter.on("log", (msg) => logEvents(msg));
 
-// fs.readFile(
-//   path.join(__dirname, "files", "starter.txt"),
-//   "utf8",
-//   (err, data) => {
-//     if (err) throw err;
-//     console.log(data);
-//   }
-// );
-// async task라는 file읽는동안 hello..를 먼저 출력해버림
-// console.log("Hello...");
-
-// fs.writeFile(
-//   path.join(__dirname, "files", "reply.txt"),
-//   "Nice to meet you.",
-//   (err) => {
-//     if (err) throw err;
-//     console.log("write complete");
-
-//     fs.appendFile(
-//       path.join(__dirname, "files", "reply.txt"),
-//       "\n\nYes it is.",
-//       (err) => {
-//         if (err) throw err;
-//         console.log("Append complete");
-
-//         fs.rename(
-//           path.join(__dirname, "files", "reply.txt"),
-//           path.join(__dirname, "files", "newReply.txt"),
-//           (err) => {
-//             if (err) throw err;
-//             console.log("Rename complete");
-//           }
-//         );
-//       }
-//     );
-//   }
-// );
-
-// exit on uncaught errors
-process.on("uncaughtException", (err) => {
-  console.error(`There was an uncaught exception: ${err}`);
-  process.exit(1);
-});
-/*
-/Users/gslee/.nvm/versions/node/v18.16.0/bin/node ./index.js
-Process exited with code 1
-There was an uncaught exception: Error: ENOENT: no such file or directory, open './files/askd.txt'
-*/
+setTimeout(() => {
+  // Emit event
+  myEmitter.emit("log", "Log event emitted!");
+}, 2000);
