0aa4bf9346dbf2d778f0d8d0fad07a939ac35680
lgswin <lgswin@gmail.com>
2023-11-09 22:02:05 -0500

    contentType handling, redirection, error handling, log file

--- logEvents.js
+++ logEvents.js
@@ -5,19 +5,19 @@ const fs = require("fs");
 const fsPromises = require("fs").promises;
 const path = require("path");
 
-const logEvents = async (message) => {
+const logEvents = async (message, logName) => {
   const dateTime = `${format(new Date(), "yyyyMMdd\tHH:mm:ss\t")}`;
   const logItem = `${dateTime}\y${uuid()}\t${message}\n`;
   console.log(logItem);
+
   try {
     if (!fs.existsSync(path.join(__dirname, "logs"))) {
       await fsPromises.mkdir(path.join(__dirname, "logs"));
     }
+
+    console.log(`path: ${path} : ${logName}`);
     // testing
-    await fsPromises.appendFile(
-      path.join(__dirname, "logs", "eventLog.txt"),
-      logItem
-    );
+    await fsPromises.appendFile(path.join(__dirname, "logs", logName), logItem);
   } catch (err) {
     console.log(err);
   }
--- server.js
+++ server.js
@@ -1,34 +1,116 @@
-const os = require("os");
+const http = require("http");
 const path = require("path");
+const fs = require("fs");
+const fsPromises = require("fs").promises;
 
-console.log(os.type());
-console.log(os.hostname());
-console.log(os.platform());
-console.log(os.version());
-console.log(os.homedir());
+const logEvents = require("./logEvents");
+const EventEmitter = require("events");
+class Emitter extends EventEmitter {}
+const myEmitter = new Emitter();
+myEmitter.on("log", (msg, fileName) => logEvents(msg, fileName));
 
-console.log(__dirname);
-console.log(__filename);
+const PORT = process.env.PORT || 3500;
 
-console.log(path.dirname(__filename));
-console.log(path.basename(__filename));
-console.log(path.extname(__filename));
+const serveFile = async (filePath, contentType, response) => {
+  try {
+    const rawData = await fsPromises.readFile(
+      filePath,
+      !contentType.includes("image") ? "utf8" : ""
+    );
+    const data =
+      contentType === "appliation/json" ? JSON.parse(rawData) : rawData;
+    console.log(data);
+    response.writeHead(filePath.includes("404.html") ? 404 : 200, {
+      "Content-Type": contentType,
+    });
+    response.end(
+      contentType === "appliation/json" ? JSON.stringify(data) : data
+    );
+  } catch (err) {
+    console.log(err);
+    myEmitter.emit("log", `${err.name}: ${err.message}`, "errLog.txt");
+    response.statusCode = 500;
+    response.end();
+  }
+};
 
-console.log(path.parse(__filename));
+const server = http.createServer((req, res) => {
+  console.log(req.url, req.method);
+  myEmitter.emit("log", `${req.url}\t${req.method}`, "reqLog.txt");
+  /*
+  let path;
 
-const pathValue = path.parse(__filename);
+  if (req.url === "/" || req.url === "index.html") {
+    res.statusCode = 200;
+    res.setHeader("Content-Type", "text/html");
+    path = path.join(__dirname, "views", "index.html");
+    fs.readFile(path, "utf8", (err, data) => {
+      res.end(data);
+    });
+  }
+  */
 
-console.log(pathValue);
+  const extension = path.extname(req.url);
+  let contentType;
+  switch (extension) {
+    case ".css":
+      contentType = "text/css";
+      break;
+    case ".js":
+      contentType = "text/javascript";
+      break;
+    case ".json":
+      contentType = "application/json";
+      break;
+    case ".jpg":
+      contentType = "image/jpeg";
+      break;
+    case ".png":
+      contentType = "image/png";
+      break;
+    case ".txt":
+      contentType = "text/plain";
+      break;
+    default:
+      contentType = "text/html";
+  }
 
-Object.keys(pathValue).forEach((key) =>
-  console.log(key + " : " + pathValue[key])
-);
+  let filePath =
+    contentType === "text/html" && req.url === "/"
+      ? path.join(__dirname, "views", "index.html")
+      : contentType === "text/html" && req.url.slice(-1) === "/"
+      ? path.join(__dirname, "views", req.url, "index.html")
+      : contentType === "text/html"
+      ? path.join(__dirname, "views", req.url)
+      : path.join(__dirname, req.url);
 
-const math = require("./math");
-console.log(math.add(2, 3));
+  // makes .html extension not required in the browser
+  if (!extension && req.url.slice(-1) !== "/") filePath += ".html";
 
-const { add, subtract, multiply, divide } = require("./math");
-console.log(add(10, 3));
-console.log(subtract(10, 3));
-console.log(multiply(10, 3));
-console.log(divide(10, 3));
+  const fileExists = fs.existsSync(filePath);
+
+  if (fileExists) {
+    // serve the file
+    serveFile(filePath, contentType, res);
+  } else {
+    // 404
+    // 301 redirect
+    switch (path.parse(filePath).base) {
+      case "old-page.html":
+        res.writeHead(301, { Location: "/new-page.html" });
+        res.end();
+        break;
+      case "www-page.html":
+        res.writeHead(301, { Location: "/" });
+        res.end();
+        break;
+      default:
+        // serve a 404 response
+        serveFile(path.join(__dirname, "views", "404.html"), "text/html", res);
+    }
+  }
+});
+
+server.listen(PORT, (req, res) => {
+  console.log(`Server running on port ${PORT}`);
+});
